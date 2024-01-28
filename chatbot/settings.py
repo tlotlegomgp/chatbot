4e0cc0c4861f538f28a86af772c7e6344fb42c3d
Tlotlego <tlotlego.mogapi@kazang.com>
2024-01-28 19:30:47 +0200

    Installed project requirements

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api',
+    'index',
+    
+    'rest_framework'
+
 ]
 
 MIDDLEWARE = [
